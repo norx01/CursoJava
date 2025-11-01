1f0bc0442a7fc91c7acc321f9a29f594a93b883a
norx <juan_pinillos@hotmail.com>
2025-11-01 10:53:48 -0500

    GUI v1

--- src/gui/CalculadoraGUI.java
+++ src/gui/CalculadoraGUI.java
@@ -4,30 +4,44 @@ import javax.swing.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 
-public class CalculadoraGUI {
+public class CalculadoraGUI
+{
     private JPanel mainPanel;
-    private JTextField textField1;
-    private JTextField textField2;
-    private JButton button1;
+    private JTextField campo1;
+    private JTextField campo2;
+    private JButton sumarButton;
+    private JLabel etiquetaResultado;
+
 
     public CalculadoraGUI()
     {
-        button1.addActionListener(new ActionListener() {
+
+        sumarButton.addActionListener(new ActionListener() {
             @Override
             public void actionPerformed(ActionEvent e)
             {
-
+                int num1 = Integer.parseInt(campo1.getText());
+                int num2 = Integer.parseInt(campo2.getText());
+                int resultado = num1+num2;
+                //JOptionPane.showMessageDialog(null, "La suma es: "+resultado);
+                etiquetaResultado.setText("El resultado es: "+resultado);
             }
         });
     }
 
     public static void main(String[] args)
     {
+        //Crea el marco de la interfaz grafica
         JFrame frame = new JFrame("Calculadora");
+        //Agrega el panel al marco
         frame.setContentPane(new CalculadoraGUI().mainPanel);
+        //Al cerrar la ventana se cierra la aplicacion
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        //Organiza el marco
         frame.pack();
+        //Muestra el marco
         frame.setVisible(true);
+        //Define el tamaño del marco
         frame.setSize(500,500);
     }
 
